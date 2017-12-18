4df5fb9261e7356fa7cb2b852cacbaaf2bb18f68
LauraAWard <l-ward@northwestern.edu>
2017-12-18 02:58:45 -0600

    Added ClearDB config

--- config/connection.js
+++ config/connection.js
@@ -4,9 +4,9 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   port: process.env.PORT || 3306,
   host: env("DB_HOST", "us-cdbr-iron-east-05.cleardb.net"),
-  user: env("DB_DATABASE", "***REMOVED***"),
-  password: env("DB_USERNAME", "***REMOVED***"),
-  database: env("DB_PASSWORD", "***REMOVED***")
+  user: env("DB_USERNAME", "***REMOVED***"),
+  password: env("DB_PASSWORD", "***REMOVED***"),
+  database: env("DB_DATABASE", "***REMOVED***")
 });
 
 //connect
