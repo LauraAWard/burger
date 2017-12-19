8ffcf16caddaab48c2830540590dcb2fc404e138
Laura A Ward <law446@ads.northwestern.edu>
2017-12-18 18:43:48 -0600

    added JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 //set up MySQL connection
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: process.env.MYSQL_ROOT_PWD,
-  database: "burgers_db"
-});
+  password: "hacktheplanet",
+  database: "todoagain_db"
+  });
+};
 
 //connect
 connection.connect(function(err) {
